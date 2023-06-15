091e1c8fc0ca7a0da6101fe07154435bf9adb853
Julian Schleemann <julian.schleemann@atmina.de>
2023-06-15 13:43:26 +0200

    feat: add support for twMerge

--- eslint/tailwind.js
+++ eslint/tailwind.js
@@ -14,6 +14,12 @@ module.exports = {
     ...recommended.rules,
     'tailwindcss/no-custom-classname': 'off',
   },
+  // https://github.com/francoismassart/eslint-plugin-tailwindcss#more-settings
+  settings: {
+    tailwindcss: {
+      callees: ['classNames', 'clsx', 'ctl', 'twMerge'],
+    }
+  },
   languageOptions: {
     parserOptions: recommended.parserOptions,
   },
