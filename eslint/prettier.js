b392192dfda59b158ed9e733a05f13a65cf22db4
Julian Schleemann <julian.schleemann@atmina.de>
2023-09-22 11:34:11 +0200

    chore!: drop eslint-plugin-prettier

--- eslint/prettier.js
+++ eslint/prettier.js
@@ -1,23 +1,12 @@
-// Runs Prettier as an ESLint rule and reports differences as individual ESLint issues.
-const prettierPlugin = require('eslint-plugin-prettier');
-
 // Turns off all rules that are unnecessary or might conflict with Prettier.
 const prettierOverrides = require('eslint-config-prettier');
-
-const prettierOptions = require('../prettier');
 
 /**
  * @type {import('eslint').Linter.FlatConfig}
  */
 module.exports = {
   files: ['**/*.{ts,tsx,js,jsx}'],
-  plugins: {prettier: prettierPlugin},
   rules: {
     ...prettierOverrides.rules,
-    ...prettierPlugin.configs.recommended.rules,
-    // Note: this effectively locks in our Prettier configuration (ignoring
-    // .prettierrc completely). Since this is an opinionated setup, we probably
-    // don't want to allow customization anyway.
-    'prettier/prettier': ['warn', prettierOptions],
   },
 };
